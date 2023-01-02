23822b85a5a8c696f20c6bc672e117d31d22460b
esraa <israamohamed212100@gmail.com>
2023-01-02 17:46:59 +0200

    new log

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
 import React from "react";
 import Arry from "./Components/Arry";
-import Comp from "./Components/State";
 
-// import{Routes,Route, BrowserRouter} from"react-router-dom"
-import Test from "./Components/Test";
+
+
 
 function App() {
 
@@ -16,15 +14,7 @@ function App() {
 </div>
  )
 }
-// <BrowserRouter>
-//      <Routes>
 
-// <Route path="/" element={<Test/>}></Route>
-
-
-
-// </Routes>
-// </BrowserRouter>
 
 
 
